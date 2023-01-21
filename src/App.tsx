a1e1322aeab08ef8814dba0bf5b1e35bb63d1afb
KAST13L <o4enbsmeshno@gmail.com>
2023-01-21 20:56:06 +0200

    feat: added app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import {
+    AppBar,
+    Box,
+    Button,
+    CssBaseline,
+    IconButton,
+    Toolbar,
+    Typography
+} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 export const App = () => {
     return (
-        <div>
-          App
-        </div>
+        <>
+            <CssBaseline/>
+            <Box sx={{flexGrow: 1}}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{mr: 2}}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                            Eneyida
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+                <h1>Eneyida TV</h1>
+            </Box>
+        </>
     );
 };
 
