df164db148c032bee75dbfbd7699d5154660cbd8
KAST13L <o4enbsmeshno@gmail.com>
2023-01-22 14:42:46 +0200

    feat: added menu on left side

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,61 @@ import {
     AppBar,
     Box,
     Button,
-    CssBaseline, Drawer,
+    CssBaseline,
+    Divider,
+    Drawer, Hidden,
     IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
     Toolbar,
     Typography
 } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu'
 
 export const App = () => {
     const [IsDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
+
+
+    const list = () => (
+        <Box
+            sx={{width: '250px'}}
+            role="presentation"
+            onClick={() => setIsDrawerOpen(false)}
+            onKeyDown={() => setIsDrawerOpen(false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider/>
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
 
     return (
         <>
@@ -19,16 +65,18 @@ export const App = () => {
             <Box>
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton
-                            onClick={() => setIsDrawerOpen(true)}
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{mr: 2}}
-                        >
-                            <MenuIcon/>
-                        </IconButton>
+                        <Hidden only={['xl', 'lg']}>
+                            <IconButton
+                                onClick={() => setIsDrawerOpen(true)}
+                                size="large"
+                                edge="start"
+                                color="inherit"
+                                aria-label="menu"
+                                sx={{mr: 2}}
+                            >
+                                <MenuIcon/>
+                            </IconButton>
+                        </Hidden>
                         <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                             Eneyida
                         </Typography>
@@ -41,7 +89,7 @@ export const App = () => {
                     open={IsDrawerOpen}
                     onClose={() => setIsDrawerOpen(false)}
                 >
-                    +++++++++ menu ++++++++
+                    {list()}
                 </Drawer>
             </Box>
         </>
