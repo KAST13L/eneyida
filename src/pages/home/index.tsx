a2847660eb8492cacfda28fe140d7ab7335330e1
KAST13L <o4enbsmeshno@gmail.com>
2023-01-25 20:14:32 +0200

    fix: ui

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import {Box, Grid, Paper, styled} from "@mui/material";
 
 export const Home = () => {
+
+    const SelectedMovies = styled(Paper)(({theme}) => ({
+        backgroundColor: '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        color: theme.palette.text.secondary,
+        height: 'calc(100vh - 130px)',
+        position: 'sticky',
+        top: theme.spacing(2)
+    }))
+
     return (
-        <div>
-            Home
-        </div>
+        <Box sx={{flexGrow:1, marginTop:2}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Paper elevation={5}>
+                        filters section
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Paper elevation={5}>
+                        List of movies
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Paper elevation={5}>
+                        <SelectedMovies/>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Box>
     );
 };
 
