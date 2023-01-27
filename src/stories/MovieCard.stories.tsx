2b1768c70c90d61e6dc3a686eb37672e1884dfd5
KAST13L <o4enbsmeshno@gmail.com>
2023-01-27 19:31:00 +0200

    fix: add props to component and install sb library

---
+++ src/stories/MovieCard.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {MovieCard} from "../components";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {action} from "@storybook/addon-actions";
+import {movies} from "./stub";
+
+export default {
+    title: 'Example/MovieCard',
+    component: MovieCard,
+    argTypes: {
+        onSelectClick: action('film selected')
+    }
+} as ComponentMeta<typeof MovieCard>;
+
+const Template: ComponentStory<typeof MovieCard> = (args) => <MovieCard {...args}/>;
+
+export const View = Template.bind({});
+View.args = {
+    movie: movies[0],
+}
+
+
