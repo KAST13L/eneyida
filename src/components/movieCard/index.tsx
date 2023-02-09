b617a6a95546fb26e0aed82018ee3785ba1720c0
KAST13L <o4enbsmeshno@gmail.com>
2023-02-09 19:01:14 +0200

    fix: change add name instruction

--- src/components/movieCard/index.tsx
+++ src/components/movieCard/index.tsx
@@ -22,7 +22,7 @@ export const MovieCard: React.FC<MovieCardPropsType> = ({movie, onSelectClick}) 
         <Card sx={{maxWidth: 250, position: 'relative'}}>
             <RightTopButtonMenu callback={(movie: MovieType) => onSelectClick(movie)}
                                 movie={movie}/>
-            <Tooltip  title={'Add'} followCursor enterDelay={500} enterNextDelay={500} >
+            <Tooltip title={'Click to Add'} followCursor enterDelay={500} enterNextDelay={500} >
                 <CardMedia
                     component="img"
                     height="300"
