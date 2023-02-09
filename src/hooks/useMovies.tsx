985321952923d118949f76feccc051a8f3f91bf5
KAST13L <o4enbsmeshno@gmail.com>
2023-02-09 13:23:28 +0200

    feat: added new constants

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,6 +1,8 @@
 import React, {useCallback, useState} from 'react';
 import {MovieType} from "../stories/stub";
 
+const MAX_SELECTED_MOVIES = 20;
+
 export const useMovies = () => {
 
     const [selectedMovies, setSelectedMovies] = useState<MovieType[]>([])
@@ -8,16 +10,16 @@ export const useMovies = () => {
     const selectMovie = useCallback((movie: MovieType) => {
 
         const selectedMoviesLength = selectedMovies.length
-        const isNewMovie = !selectedMovies.find(({title}) => title === movie.title )
+        const isNewMovie = !selectedMovies.find(({title}) => title === movie.title)
 
-        if (isNewMovie && selectedMoviesLength < 20) {
+        if (isNewMovie && selectedMoviesLength < MAX_SELECTED_MOVIES) {
             setSelectedMovies([movie, ...selectedMovies])
         }
 
     }, [selectedMovies])
 
     const deleteMovie = useCallback((movie: MovieType) => {
-        setSelectedMovies(selectedMovies.filter(({title}) => title !== movie.title ))
+        setSelectedMovies(selectedMovies.filter(({title}) => title !== movie.title))
     }, [selectedMovies])
 
 
