0c68395219285db66f8e2549e507449742330bbe
KAST13L <o4enbsmeshno@gmail.com>
2023-02-12 15:11:55 +0200

    feat: test useMovies hook

--- src/hooks/useMovies/useMovies.test.ts
+++ src/hooks/useMovies/useMovies.test.ts
@@ -7,12 +7,38 @@ describe('test useMovies hook', () => {
     const basicMovie = movies[0]
 
     it('should select movie', () => {
-        const { result } = renderHook(() => useMovies())
+        const {result} = renderHook(() => useMovies())
         act(() => {
             result.current.selectMovie(basicMovie)
         })
         expect(result.current.selectedMovies[0].title).toBe('Title movie')
-        expect(result.current.selectedMovies[0].adult).toBeTruthy();
         expect(result.current.selectedMovies.length).toBe(1)
-    })
+    });
+
+    it('should delete movie', () => {
+        const {result} = renderHook(() => useMovies())
+        act(() => {
+            result.current.selectMovie(basicMovie)
+        })
+        expect(result.current.selectedMovies.length).toBeTruthy()
+
+        act(() => {
+            result.current.deleteMovie(basicMovie)
+        })
+        expect(result.current.selectedMovies.length).toBeFalsy()
+    });
+
+    it('should select movie only once', () => {
+        const {result} = renderHook(() => useMovies())
+        act(() => {
+            result.current.selectMovie(basicMovie)
+        })
+        expect(result.current.selectedMovies.length).toBe(1)
+
+        act(() => {
+            result.current.selectMovie(basicMovie)
+        })
+        expect(result.current.selectedMovies.length).toBe(1)
+    });
+
 })
