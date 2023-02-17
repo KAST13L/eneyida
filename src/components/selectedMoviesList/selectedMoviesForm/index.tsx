2b63808316277d4d5fef3112cc2ed7b1a487ffe1
KAST13L <o4enbsmeshno@gmail.com>
2023-02-17 23:35:41 +0200

    feat: add react final form

--- src/components/selectedMoviesList/selectedMoviesForm/index.tsx
+++ src/components/selectedMoviesList/selectedMoviesForm/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import {Box, Button, TextField} from "@mui/material";
+import {Field, Form} from 'react-final-form'
 
 export const SelectedMoviesForm = () => {
     return (
-        <Box sx={{display: 'flex'}}>
-            <TextField variant={'outlined'} fullWidth
-                       placeholder={'enter a title for the movie list'}/>
-            <Button variant={'contained'}>OK</Button>
-        </Box>
+        <Form
+            onSubmit={() => {}}
+            render={({handleSubmit, form, values}) => (
+                <form onSubmit={handleSubmit}>
+                    <Field name="titleOfList">
+                        {({input, meta}) => (
+                            <Box sx={{display: 'flex'}}>
+                                <TextField {...input} variant={'outlined'} fullWidth
+                                           placeholder={'enter a title for the movie list'}/>
+                                <Button variant={'contained'}>OK</Button>
+                                {meta.error && meta.touched &&
+                                    <span>{meta.error}</span>}
+                            </Box>
+                        )}
+                    </Field>
+                </form>
+            )}
+        />
     );
 };
 
