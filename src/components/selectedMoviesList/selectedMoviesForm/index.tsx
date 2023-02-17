8ec43d4975328a9bf6492b8efbf7196702fdcf3b
KAST13L <o4enbsmeshno@gmail.com>
2023-02-17 23:06:44 +0200

    fix: change css styles

--- src/components/selectedMoviesList/selectedMoviesForm/index.tsx
+++ src/components/selectedMoviesList/selectedMoviesForm/index.tsx
@@ -3,7 +3,7 @@ import {Box, Button, TextField} from "@mui/material";
 
 export const SelectedMoviesForm = () => {
     return (
-        <Box sx={{display: 'flex', margin: 1}}>
+        <Box sx={{display: 'flex'}}>
             <TextField variant={'outlined'} fullWidth
                        placeholder={'enter a title for the movie list'}/>
             <Button variant={'contained'}>OK</Button>
