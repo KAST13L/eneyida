f8887275fa6459eba2dfead69b58462ecb6dcfaf
KAST13L <o4enbsmeshno@gmail.com>
2023-02-17 22:46:48 +0200

    feat: encapsulation

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Box, Grid, Paper} from "@mui/material";
-import {useQuery} from "@apollo/client";
-import {MOVIES_QUERY} from "./queries";
-import {MovieCard} from "../../components";
-import Stack from "@mui/material/Stack";
-import Pagination from "@mui/material/Pagination";
-import {useMovies} from "../../hooks/useMovies/useMovies";
-import {MovieType} from "../../types/types";
 import {SelectedMoviesList} from "../../components/selectedMoviesList";
+import {MoviesList} from "../../components/moviesList";
 
 
 export const Home = () => {
-
-    const [page, setPage] = useState(1)
-    const {loading, error, data} = useQuery(MOVIES_QUERY, {variables: {page}})
-    const {selectMovie} = useMovies()
-
-    if (error) {
-        return <div>ERROR</div>
-    }
 
     return (
         <Box sx={{flexGrow: 1, marginTop: 2}}>
@@ -31,30 +17,7 @@ export const Home = () => {
                 </Grid>
 
                 <Grid item xs={12} md={8}>
-                    <Paper elevation={5}>
-                        <Box sx={{flexGrow: 1, padding: 1}}>
-                            <Grid container spacing={2}>
-                                {
-                                    loading && "Loading..."
-                                }
-                                {
-                                    data && data.movies.results.map((el: MovieType) =>
-                                        <Grid item xs={12} sm={6} md={4} lg={3}
-                                              key={el.id}>
-                                            <MovieCard movie={{...el}}
-                                                       onSelectClick={selectMovie}/>
-                                        </Grid>)
-                                }
-                                <Stack spacing={2} sx={{margin: 3}}>
-                                    <Pagination
-                                        count={500}
-                                        page={page}
-                                        onChange={(event, page) => setPage(page)}
-                                    />
-                                </Stack>
-                            </Grid>
-                        </Box>
-                    </Paper>
+                    <MoviesList/>
                 </Grid>
 
                 <Grid item xs={12} md={4}>
