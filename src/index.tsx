7b78963c89f5b55b46b11fb71281a9bb13aaa293
KAST13L <o4enbsmeshno@gmail.com>
2023-02-21 21:46:26 +0200

    fix: change hash to browser router because useSearchParams work noy correct

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import {App} from './App';
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {GraphQlServer} from "./graphQlServer";
 
 
