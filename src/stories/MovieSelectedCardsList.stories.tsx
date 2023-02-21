e7fe09ef2729341fb9ad98efadc72bf93a09e685
KAST13L <o4enbsmeshno@gmail.com>
2023-02-21 20:37:05 +0200

    fix: encapsulation

--- src/stories/MovieSelectedCardsList.stories.tsx
+++ src/stories/MovieSelectedCardsList.stories.tsx
@@ -1,34 +1,18 @@
 import React from 'react';
 import {ComponentMeta, ComponentStory} from "@storybook/react";
 import {action} from "@storybook/addon-actions";
-import {
-    ApolloClient,
-    ApolloProvider,
-    createHttpLink,
-    InMemoryCache
-} from "@apollo/client";
 import {HashRouter} from "react-router-dom";
 import {SelectedCardMoviesList} from "../components/selectedMoviesList";
 import {movies} from "./stub";
+import {GraphQlServer} from "../graphQlServer";
 
 export default {
     title: 'List/ SelectedCardMoviesList',
     component: SelectedCardMoviesList
 } as ComponentMeta<typeof SelectedCardMoviesList>;
 
-const httpLink = createHttpLink({
-    uri: 'http://localhost:4000'
-});
-const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
-    connectToDevTools: true
-});
-
 const Template: ComponentStory<typeof SelectedCardMoviesList> = (args) => <HashRouter>
-    <ApolloProvider client={client}>
-        <SelectedCardMoviesList {...args}/>
-    </ApolloProvider>,
+    <GraphQlServer component={<SelectedCardMoviesList {...args}/>}/>
 </HashRouter>;
 
 export const View = Template.bind({});
