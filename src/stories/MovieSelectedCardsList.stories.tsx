b176fdf37c3d4e16840b6b24ff748a582fd6d1c4
KAST13L <o4enbsmeshno@gmail.com>
2023-02-21 20:25:16 +0200

    feat: added new stories

--- src/stories/MovieSelectedCardsList.stories.tsx
+++ src/stories/MovieSelectedCardsList.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {ComponentMeta, ComponentStory} from "@storybook/react";
-import {MoviesList} from '../components/moviesList';
 import {action} from "@storybook/addon-actions";
 import {
     ApolloClient,
@@ -9,11 +8,13 @@ import {
     InMemoryCache
 } from "@apollo/client";
 import {HashRouter} from "react-router-dom";
+import {SelectedCardMoviesList} from "../components/selectedMoviesList";
+import {movies} from "./stub";
 
 export default {
-    title: 'List/ MoviesList',
-    component: MoviesList
-} as ComponentMeta<typeof MoviesList>;
+    title: 'List/ SelectedCardMoviesList',
+    component: SelectedCardMoviesList
+} as ComponentMeta<typeof SelectedCardMoviesList>;
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
@@ -24,15 +25,16 @@ const client = new ApolloClient({
     connectToDevTools: true
 });
 
-const Template: ComponentStory<typeof MoviesList> = (args) => <HashRouter>
+const Template: ComponentStory<typeof SelectedCardMoviesList> = (args) => <HashRouter>
     <ApolloProvider client={client}>
-        <MoviesList {...args}/>
+        <SelectedCardMoviesList {...args}/>
     </ApolloProvider>,
 </HashRouter>;
 
 export const View = Template.bind({});
 View.args = {
-    selectMovie: action('select movie')
+    deleteMovie: action('delete movie'),
+    selectedMovies: movies
 }
 
 
