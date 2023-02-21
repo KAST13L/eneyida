3ff51d448219caef4f2ac30312cd80ce9ec4c372
KAST13L <o4enbsmeshno@gmail.com>
2023-02-21 22:43:51 +0200

    fix: change hash to browser router because useSearchParams work noy correct

--- src/pages/recommend/index.tsx
+++ src/pages/recommend/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSearchParams} from "react-router-dom";
+import Typography from "@mui/material/Typography";
+
+interface SearchParamsType {
+    ids?: string[] | null,
+    title: string | null
+}
 
 export const Recommend = () => {
 
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
+    const [params, setParams] = useState<SearchParamsType>({
+        ids: [],
+        title: ''
+    })
+
+    useEffect(() => {
+        const title = searchParams.get('title')
+        const ids = searchParams.get('ids')
+
+        setParams({
+            ids: ids?.split(','),
+            title: title
+        })
+
+    }, [searchParams])
 
     return (
-        <div>
-            {searchParams}
-        </div>
+        <>
+            <Typography variant={'h2'} component={'div'}>
+                {params.title}
+            </Typography>
+        </>
     );
 };
