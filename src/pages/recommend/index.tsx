fe66941c332407362433b8389f3d42efe4c033ce
KAST13L <o4enbsmeshno@gmail.com>
2023-02-21 22:49:18 +0200

    fix: change hash to browser router because useSearchParams work noy correct

--- src/pages/recommend/index.tsx
+++ src/pages/recommend/index.tsx
@@ -1,35 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {useSearchParams} from "react-router-dom";
 import Typography from "@mui/material/Typography";
+import {useQuery} from "@apollo/client";
+import {MOVIES_BY_IDS_QUERY} from "./queries";
 
-interface SearchParamsType {
-    ids?: string[] | null,
-    title: string | null
-}
 
 export const Recommend = () => {
 
     const [searchParams] = useSearchParams();
-    const [params, setParams] = useState<SearchParamsType>({
-        ids: [],
-        title: ''
-    })
+    const {
+        loading,
+        error,
+        data
+    } = useQuery(MOVIES_BY_IDS_QUERY, {variables: {ids: searchParams.get('ids')?.split(',')}})
 
-    useEffect(() => {
-        const title = searchParams.get('title')
-        const ids = searchParams.get('ids')
-
-        setParams({
-            ids: ids?.split(','),
-            title: title
-        })
-
-    }, [searchParams])
+    if (error) {
+        return <div>ERROR</div>
+    }
+    if (loading) {
+        return <div>Loading...</div>
+    }
 
     return (
         <>
             <Typography variant={'h2'} component={'div'}>
-                {params.title}
+                {searchParams.get('title')}
             </Typography>
         </>
     );
