007860ebde451ac4275b9393e18076fb4419302b
KAST13L <o4enbsmeshno@gmail.com>
2023-02-21 23:09:13 +0200

    feat: create new component

--- src/pages/recommend/index.tsx
+++ src/pages/recommend/index.tsx
@@ -3,6 +3,9 @@ import {useSearchParams} from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import {useQuery} from "@apollo/client";
 import {MOVIES_BY_IDS_QUERY} from "./queries";
+import {MovieType} from "../../types/types";
+import {Grid} from "@mui/material";
+import {MovieCardRecommend} from "../../components/MovieCardRecommend";
 
 
 export const Recommend = () => {
@@ -12,7 +15,7 @@ export const Recommend = () => {
         loading,
         error,
         data
-    } = useQuery(MOVIES_BY_IDS_QUERY, {variables: {ids: searchParams.get('ids')?.split(',')}})
+    } = useQuery(MOVIES_BY_IDS_QUERY, {variables: {ids: searchParams.get('ids')?.split(',').map((id) => +id)}})
 
     if (error) {
         return <div>ERROR</div>
@@ -23,9 +26,18 @@ export const Recommend = () => {
 
     return (
         <>
-            <Typography variant={'h2'} component={'div'}>
+            <Typography variant={'h2'} component={'div'} sx={{p: 3}}>
                 {searchParams.get('title')}
             </Typography>
+            <Grid container spacing={2}>
+                {
+                    data.moviesByIds && data.moviesByIds.map((el: MovieType) =>
+                        <Grid item xs={12} sm={6} md={4} lg={3}
+                              key={el.id}>
+                            <MovieCardRecommend movie={{...el}}/>
+                        </Grid>)
+                }
+            </Grid>
         </>
     );
 };
