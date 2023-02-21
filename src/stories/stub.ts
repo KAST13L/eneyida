9773df1034e12020f3f2f673b10e38925a3fd51c
KAST13L <o4enbsmeshno@gmail.com>
2023-02-21 20:04:05 +0200

    fix: change url in stub for stories

--- src/stories/stub.ts
+++ src/stories/stub.ts
@@ -8,7 +8,7 @@ export const movies: MovieType[] = [{
     }],
     adult: true,
     id: 'id12',
-    posterPath: 'posetr Path',
+    posterPath: 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/WOPA160517_D056-resized.jpg?crop=864%2C0%2C1728%2C2304&wid=600&hei=800&scl=2.88',
     releaseDate: 'release date',
     originalLanguage: 'originalLanguage',
     originalTitle: 'originalTitle',
