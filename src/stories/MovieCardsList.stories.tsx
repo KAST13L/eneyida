230599a2a7d310ef0a6df8114846b9cc67f268e0
KAST13L <o4enbsmeshno@gmail.com>
2023-03-02 13:26:34 +0200

    fix: update stories

--- src/stories/MovieCardsList.stories.tsx
+++ src/stories/MovieCardsList.stories.tsx
@@ -4,15 +4,19 @@ import {MoviesList} from '../components/moviesList';
 import {action} from "@storybook/addon-actions";
 import {GraphQlServer} from "../graphQlServer";
 import {AppContextProvider} from "../providers/context";
+import {BrowserRouter} from "react-router-dom";
 
 export default {
     title: 'List/ MoviesList',
     component: MoviesList
 } as ComponentMeta<typeof MoviesList>;
 
-const Template: ComponentStory<typeof MoviesList> = (args) => <AppContextProvider>
-    <GraphQlServer component={<MoviesList {...args}/>}/>
-</AppContextProvider>
+const Template: ComponentStory<typeof MoviesList> = (args) =>
+    <BrowserRouter>
+        <AppContextProvider>
+            <GraphQlServer component={<MoviesList {...args}/>}/>
+        </AppContextProvider>
+    </BrowserRouter>
 
 
 export const View = Template.bind({});
