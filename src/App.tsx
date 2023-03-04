41683335b2d98716b89a035d61b99898f292911d
KAST13L <o4enbsmeshno@gmail.com>
2023-03-04 19:05:09 +0200

    fix: daily refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ export const App = () => {
                 <Container maxWidth={'xl'}>
                     <Routes>
 
+
                         <Route path='*' element={<Home/>}/>
                         <Route path='settings' element={<Settings/>}/>
                         <Route path='recommend' element={<Recommend/>}/>
